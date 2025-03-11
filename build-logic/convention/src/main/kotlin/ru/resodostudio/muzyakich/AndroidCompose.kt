35ab739bda6beccaf966f710c6b570b61529bd1a
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-03-11 22:22:11 +0300

    Refactor dependencies and update Compose configuration

--- build-logic/convention/src/main/kotlin/ru/resodostudio/muzyakich/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/ru/resodostudio/muzyakich/AndroidCompose.kt
@@ -22,6 +22,8 @@ internal fun Project.configureAndroidCompose(
             val bom = libs.findLibrary("androidx-compose-bom").get()
             "implementation"(platform(bom))
             "androidTestImplementation"(platform(bom))
+            "implementation"(libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+            "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
         }
     }
 
