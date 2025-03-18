f27ca69577ce5a37fec3b8863eb9d8f65f874525
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-03-18 08:09:18 +0300

    Refactor `build-logic`

--- build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
@@ -5,7 +5,6 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.exclude
 import ru.resodostudio.muzyakich.MuzFlavor
 import ru.resodostudio.muzyakich.libs
 
--- build-logic/convention/src/main/kotlin/ru/resodostudio/muzyakich/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/ru/resodostudio/muzyakich/KotlinAndroid.kt
@@ -4,7 +4,6 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.provideDelegate
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
@@ -30,16 +29,16 @@ internal fun Project.configureKotlinAndroid(
  * Configure base Kotlin options
  */
 internal inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() = configure<T> {
-    // Treat all Kotlin warnings as errors (disabled by default)
-    // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
-    val warningsAsErrors: String? by project
+    val warningsAsErrors = providers.gradleProperty("warningsAsErrors")
+        .map { it.toBoolean() }
+        .orElse(false)
     when (this) {
         is KotlinAndroidProjectExtension -> compilerOptions
         is KotlinJvmProjectExtension -> compilerOptions
         else -> TODO("Unsupported project extension $this ${T::class}")
     }.apply {
         jvmToolchain(17)
-        allWarningsAsErrors = warningsAsErrors.toBoolean()
+        allWarningsAsErrors = warningsAsErrors
         freeCompilerArgs.addAll(
             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
         )
