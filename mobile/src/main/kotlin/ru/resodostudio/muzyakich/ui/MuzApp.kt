36e3b986b8df8ef3bf13d35c710d730f64c19d45
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-03-24 21:48:13 +0300

    Implement basic `PrimaryScrollableTabRow` with `Songs`, `Playlists`, `Albums`, and `Artists` tabs

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/MuzApp.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/MuzApp.kt
@@ -9,9 +9,17 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.PrimaryScrollableTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import ru.resodostudio.muzyakich.R
 import ru.resodostudio.muzyakich.core.designsystem.component.MuzTopAppBar
 import ru.resodostudio.muzyakich.core.designsystem.icon.MuzIcons
@@ -40,6 +48,26 @@ fun MuzApp() {
                 actionIcon = MuzIcons.Settings,
                 actionIconContentDescriptionRes = localesR.string.settings,
             )
+
+            val tabs = listOf(
+                "Songs",
+                "Playlists",
+                "Albums",
+                "Artists",
+            )
+            var selectedTabIndex by remember { mutableIntStateOf(0) }
+
+            PrimaryScrollableTabRow(
+                selectedTabIndex = selectedTabIndex,
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = { selectedTabIndex = index },
+                        text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) }
+                    )
+                }
+            }
         }
     }
 }
