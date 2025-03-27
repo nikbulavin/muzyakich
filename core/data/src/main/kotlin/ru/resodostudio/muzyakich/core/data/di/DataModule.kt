cb7fdef7485746fba3754ebf2e5420e8d46f736f
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-03-27 18:35:59 +0300

    Bind `MediaRepository` and `UserDataRepository` implementations

--- core/data/src/main/kotlin/ru/resodostudio/muzyakich/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/ru/resodostudio/muzyakich/core/data/di/DataModule.kt
@@ -4,6 +4,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ru.resodostudio.muzyakich.core.data.repository.MediaRepository
+import ru.resodostudio.muzyakich.core.data.repository.OfflineMediaRepository
 import ru.resodostudio.muzyakich.core.data.repository.UserDataRepository
 import ru.resodostudio.muzyakich.core.data.repository.offline.OfflineUserDataRepository
 
@@ -15,4 +17,9 @@ internal abstract class DataModule {
     internal abstract fun bindsUserDataRepository(
         userDataRepository: OfflineUserDataRepository,
     ): UserDataRepository
+
+    @Binds
+    internal abstract fun bindsMediaRepository(
+        mediaRepository: OfflineMediaRepository,
+    ): MediaRepository
 }
