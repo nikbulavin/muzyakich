2cd90280ece8375d827a81d72fe340ac3cae3f99
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-03-28 16:11:14 +0300

    Enhance `SongItem` with artwork display
    - Add `SubcomposeAsyncImage` to display song artwork in `SongItem`
    - Display music note icon if artwork is missing
    - Apply rounded corner shape to `SubcomposeAsyncImage` and set its size

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/library/SongItem.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/library/SongItem.kt
@@ -1,10 +1,22 @@
 package ru.resodostudio.muzyakich.ui.library
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil3.compose.SubcomposeAsyncImage
+import ru.resodostudio.muzyakich.core.designsystem.icon.MuzIcons
+import ru.resodostudio.muzyakich.core.designsystem.icon.rounded.MusicNote
 import ru.resodostudio.muzyakich.core.model.data.Song
 
 @Composable
@@ -28,5 +40,27 @@ fun SongItem(
                 overflow = TextOverflow.Ellipsis,
             )
         },
+        leadingContent = {
+            SubcomposeAsyncImage(
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                model = song.artworkUri,
+                contentDescription = null,
+                error = {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant)
+                    ) {
+                        Icon(
+                            imageVector = MuzIcons.Rounded.MusicNote,
+                            contentDescription = null,
+                            modifier = Modifier.size(32.dp),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                        )
+                    }
+                }
+            )
+        }
     )
 }
