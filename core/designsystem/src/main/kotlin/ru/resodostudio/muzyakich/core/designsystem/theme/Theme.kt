c979468d206eaff301c1f0c22088c56c47c6267b
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-06-08 20:03:50 +0300

    Provide `SharedTransitionScope` to `Theme`

--- core/designsystem/src/main/kotlin/ru/resodostudio/muzyakich/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudio/muzyakich/core/designsystem/theme/Theme.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -284,7 +285,9 @@ fun MuzTheme(
         motionScheme = MotionScheme.expressive(),
         content = {
             SharedTransitionLayout {
-                content()
+                CompositionLocalProvider(LocalSharedTransitionScope provides this) {
+                    content()
+                }
             }
         },
     )
