ad10e00f4502de430ec556bdd7ebba4070fb93af
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-06-08 19:53:51 +0300

    Implement expressive motion and shared transition

--- core/designsystem/src/main/kotlin/ru/resodostudio/muzyakich/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudio/muzyakich/core/designsystem/theme/Theme.kt
@@ -5,9 +5,13 @@ import android.app.UiModeManager
 import android.content.Context
 import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -245,6 +249,10 @@ private val highContrastDarkColorScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDarkHighContrast,
 )
 
+@OptIn(
+    ExperimentalMaterial3ExpressiveApi::class,
+    ExperimentalSharedTransitionApi::class,
+)
 @Composable
 fun MuzTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -270,10 +278,15 @@ fun MuzTheme(
         }
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = MuzTypography,
-        content = content,
+        motionScheme = MotionScheme.expressive(),
+        content = {
+            SharedTransitionLayout {
+                content()
+            }
+        },
     )
 }
 
