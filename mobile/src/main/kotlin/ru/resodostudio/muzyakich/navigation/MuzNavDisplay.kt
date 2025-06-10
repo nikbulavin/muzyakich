f24ec4bbfb6e980e9ac3a3e34ebccb6a34167ff0
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-06-10 23:06:57 +0300

    Adjust navigation transitions

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/navigation/MuzNavDisplay.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/navigation/MuzNavDisplay.kt
@@ -1,6 +1,11 @@
 package ru.resodostudio.muzyakich.navigation
 
-import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleOut
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.entry
@@ -8,14 +13,16 @@ import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
-import ru.resodostudio.muzyakich.core.designsystem.theme.LocalSharedTransitionScope
 import ru.resodostudio.muzyakich.ui.library.LibraryScreen
 import ru.resodostudio.muzyakich.ui.player.PlayerScreen
 
-@OptIn(ExperimentalSharedTransitionApi::class)
+@OptIn(
+    ExperimentalMaterial3ExpressiveApi::class,
+)
 @Composable
 fun MuzNavDisplay() {
     val backStack = rememberNavBackStack(LibraryRoute)
+    val motionScheme = MaterialTheme.motionScheme
 
     NavDisplay(
         backStack = backStack,
@@ -23,19 +30,31 @@ fun MuzNavDisplay() {
             rememberSavedStateNavEntryDecorator(),
             rememberViewModelStoreNavEntryDecorator(),
         ),
+        transitionSpec = {
+            ContentTransform(
+                fadeIn(motionScheme.defaultEffectsSpec()),
+                fadeOut(motionScheme.defaultEffectsSpec()),
+            )
+        },
+        popTransitionSpec = {
+            ContentTransform(
+                fadeIn(motionScheme.defaultEffectsSpec()),
+                scaleOut(
+                    animationSpec = motionScheme.defaultSpatialSpec(),
+                    targetScale = 0.7f,
+                ),
+            )
+        },
         entryProvider = entryProvider {
-            val sharedTransitionScope = LocalSharedTransitionScope.current
-            with(sharedTransitionScope) {
-                entry<LibraryRoute> {
-                    LibraryScreen(
-                        onNowPlayingBarClick = {
-                            backStack.add(PlayerRoute)
-                        },
-                    )
-                }
-                entry<PlayerRoute> {
-                    PlayerScreen()
-                }
+            entry<LibraryRoute> {
+                LibraryScreen(
+                    onNowPlayingBarClick = {
+                        backStack.add(PlayerRoute)
+                    },
+                )
+            }
+            entry<PlayerRoute> {
+                PlayerScreen()
             }
         },
     )
