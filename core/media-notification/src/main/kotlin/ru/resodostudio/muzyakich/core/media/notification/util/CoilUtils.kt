ce4d6308b76c2c51be21be2a4d6de1ba21d56318
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-06-22 12:33:39 +0300

    Add placeholder to notification artwork

--- core/media-notification/src/main/kotlin/ru/resodostudio/muzyakich/core/media/notification/util/CoilUtils.kt
+++ core/media-notification/src/main/kotlin/ru/resodostudio/muzyakich/core/media/notification/util/CoilUtils.kt
@@ -5,12 +5,17 @@ import android.graphics.Bitmap
 import android.net.Uri
 import coil3.ImageLoader
 import coil3.request.ImageRequest
+import coil3.request.error
+import coil3.request.placeholder
 import coil3.toBitmap
+import ru.resodostudio.muzyakich.core.media.notification.R
 
 internal suspend fun Uri.asArtworkBitmap(context: Context): Bitmap? {
     val loader = ImageLoader(context)
     val request = ImageRequest.Builder(context)
         .data(this)
+        .placeholder(R.drawable.ic_placeholder)
+        .error(R.drawable.ic_placeholder)
         .build()
 
     return loader.execute(request).image?.toBitmap()
