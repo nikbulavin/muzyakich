4ec687905efd0ac5db5d67655fc551878c976db4
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-06-30 22:45:39 +0300

    Enable StrictMode in debug builds
    StrictMode is a developer tool that detects operations you might be doing by accident, such as disk or network access on the main thread, and helps prevent them.

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/MuzApplication.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/MuzApplication.kt
@@ -1,7 +1,35 @@
 package ru.resodostudio.muzyakich
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy.Builder
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MuzApplication : Application()
+class MuzApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setStrictModePolicy()
+    }
+
+    private fun isDebuggable(): Boolean {
+        return 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+    }
+
+    /**
+     * Set a thread policy that detects all potential problems on the main thread, such as network
+     * and disk access.
+     *
+     * If a problem is found, the offending call will be logged and the application will be killed.
+     */
+    private fun setStrictModePolicy() {
+        if (isDebuggable()) {
+            StrictMode.setThreadPolicy(
+                Builder().detectAll().penaltyLog().build(),
+            )
+        }
+    }
+}
