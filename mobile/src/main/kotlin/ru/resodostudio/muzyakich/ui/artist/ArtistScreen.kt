2d30706188d85088cde6db9fb6c4a8ab44143b7d
Nikita Bulavin <nikbulavin@proton.me>
2025-08-25 10:08:04 +0300

    Add Scaffold to ArtistScreen

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/artist/ArtistScreen.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/artist/ArtistScreen.kt
@@ -1,9 +1,23 @@
 package ru.resodostudio.muzyakich.ui.artist
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import ru.resodostudio.muzyakich.core.designsystem.icon.MuzIcons
+import ru.resodostudio.muzyakich.core.designsystem.icon.rounded.ArrowBack
 import ru.resodostudio.muzyakich.ui.component.LoadingState
 
 @Composable
@@ -19,6 +33,7 @@ fun ArtistScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArtistScreen(
     artistUiState: ArtistUiState,
@@ -28,7 +43,38 @@ fun ArtistScreen(
         ArtistUiState.Error -> LoadingState()
         ArtistUiState.Loading -> LoadingState()
         is ArtistUiState.Success -> {
-            Text(text = artistUiState.artist.name)
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = artistUiState.artist.name,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = onBackClick) {
+                                Icon(
+                                    imageVector = MuzIcons.Rounded.ArrowBack,
+                                    contentDescription = null,
+                                )
+                            }
+                        },
+                    )
+                }
+            ) { paddingValues ->
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(300.dp),
+                    contentPadding = PaddingValues(
+                        top = paddingValues.calculateTopPadding() + 8.dp,
+                        bottom = 104.dp + paddingValues.calculateBottomPadding(),
+                    ),
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+
+                }
+            }
         }
     }
 }
