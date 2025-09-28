eea7c9f7a32e5ceaade3c756ebecbd1f2d762394
Nikita Bulavin <nikbulavin@proton.me>
2025-09-28 17:23:57 +0300

    Update dependencies

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/navigation/MuzNavDisplay.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/navigation/MuzNavDisplay.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
-import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
