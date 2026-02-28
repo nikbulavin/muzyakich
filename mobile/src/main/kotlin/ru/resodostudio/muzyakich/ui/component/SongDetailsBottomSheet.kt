ac58f153995c7c98d2985dce31386c74431aea2d
Nikita Bulavin <nikbulavin@proton.me>
2026-02-28 15:14:33 +0300

    Set `ContentScale.Crop` for song artwork in `SongDetailsBottomSheet`
    This commit updates the `SongDetailsBottomSheet` to use `ContentScale.Crop` for the song artwork image. This ensures that the artwork fills the specified size while maintaining its aspect ratio.

--- mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/component/SongDetailsBottomSheet.kt
+++ mobile/src/main/kotlin/ru/resodostudio/muzyakich/ui/component/SongDetailsBottomSheet.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -93,6 +94,7 @@ fun SongDetailsBottomSheet(
                         .size(256)
                         .build(),
                     contentDescription = null,
+                    contentScale = ContentScale.Crop,
                     error = {
                         Box(
                             contentAlignment = Alignment.Center,
