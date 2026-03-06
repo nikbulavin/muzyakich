c38b02242dd1cb42d2e23db389d30b625a968f46
Nikita Bulavin <nikbulavin@proton.me>
2026-03-06 15:38:58 +0300

    Update CoilUtils.kt

--- core/media-notification/src/main/kotlin/ru/resodostudio/muzyakich/core/media/notification/util/CoilUtils.kt
+++ core/media-notification/src/main/kotlin/ru/resodostudio/muzyakich/core/media/notification/util/CoilUtils.kt
@@ -6,17 +6,14 @@ import android.net.Uri
 import coil3.ImageLoader
 import coil3.request.ImageRequest
 import coil3.request.error
-import coil3.request.placeholder
 import coil3.toBitmap
 import ru.resodostudio.muzyakich.core.media.notification.R
 
 internal suspend fun Uri.asArtworkBitmap(context: Context): Bitmap? {
-    val loader = ImageLoader(context)
-    val request = ImageRequest.Builder(context)
-        .data(this)
-        .placeholder(R.drawable.ic_placeholder)
-        .error(R.drawable.ic_placeholder)
-        .build()
-
-    return loader.execute(request).image?.toBitmap()
+    return ImageLoader(context).execute(
+        ImageRequest.Builder(context)
+            .data(this)
+            .error(R.drawable.ic_placeholder)
+            .build()
+    ).image?.toBitmap()
 }
