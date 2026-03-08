b99435274706881950caeb38909b0707b7fe22b9
Nikita Bulavin <nikbulavin@proton.me>
2026-03-08 16:51:57 +0300

    Remove unused constants from `Constants.kt`
    This commit removes several unused constants related to media playback duration and repeat/shuffle mode strings to clean up the `core/common` module.
    
    Key changes:
    - Removed `DEFAULT_DURATION_MS`.
    - Removed `REPEAT_MODE` and its associated values (`REPEAT_MODE_OFF`, `REPEAT_MODE_ALL`, `REPEAT_MODE_ONE`).
    - Removed `SHUFFLE_MODE` and its associated values (`SHUFFLE_MODE_ON`, `SHUFFLE_MODE_OFF`).

--- core/common/src/main/kotlin/ru/resodostudio/muzyakich/core/common/Constants.kt
+++ core/common/src/main/kotlin/ru/resodostudio/muzyakich/core/common/Constants.kt
@@ -6,14 +6,4 @@ object Constants {
     const val DEFAULT_MEDIA_ID = ""
     const val DEFAULT_INDEX = 0
     const val DEFAULT_POSITION_MS = 0L
-    const val DEFAULT_DURATION_MS = 0L
-
-    const val REPEAT_MODE = "repeat_mode"
-    const val REPEAT_MODE_OFF = "repeat_mode_off"
-    const val REPEAT_MODE_ALL = "repeat_mode_all"
-    const val REPEAT_MODE_ONE = "repeat_mode_one"
-
-    const val SHUFFLE_MODE = "shuffle_mode"
-    const val SHUFFLE_MODE_ON = "shuffle_mode_on"
-    const val SHUFFLE_MODE_OFF = "shuffle_mode_off"
 }
