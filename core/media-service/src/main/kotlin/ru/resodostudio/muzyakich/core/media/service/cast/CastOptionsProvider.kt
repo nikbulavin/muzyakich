1fc58c0b57da4c0018a172861f3a5e31cfe918f9
Nikita Bulavin <nikbulavin@proton.me>
2026-03-12 08:33:18 +0300

    Implement custom `CastOptionsProvider` for media service
    This commit introduces a custom `CastOptionsProvider` to replace the default Media3 implementation, allowing for more specific control over Google Cast configuration.
    
    Key changes:
    - Created `CastOptionsProvider.kt` to configure `CastOptions` with disabled media sessions and null notification options.
    - Updated `AndroidManifest.xml` to use the new `CastOptionsProvider` for the Google Cast framework.

---
+++ core/media-service/src/main/kotlin/ru/resodostudio/muzyakich/core/media/service/cast/CastOptionsProvider.kt
@@ -0,0 +1,25 @@
+package ru.resodostudio.muzyakich.core.media.service.cast
+
+import android.content.Context
+import com.google.android.gms.cast.CastMediaControlIntent
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
+
+internal class CastOptionsProvider : OptionsProvider {
+
+    override fun getCastOptions(context: Context): CastOptions {
+        val castMediaOptions = CastMediaOptions.Builder()
+            .setMediaSessionEnabled(false)
+            .setNotificationOptions(null)
+            .build()
+
+        return CastOptions.Builder()
+            .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+            .setCastMediaOptions(castMediaOptions)
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(context: Context): List<SessionProvider?>? = null
+}
