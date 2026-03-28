8fefcec7a0e60bfc8869ac16884fc1b7a209c761
Nikita Bulavin <nikbulavin@proton.me>
2026-03-28 18:08:26 +0300

    Update application version to 1.0.0-alpha05
    This commit increments the version code and updates the version name for the mobile application in the build configuration.
    
    Key changes:
    - Updated `versionCode` from `4` to `5`.
    - Updated `versionName` from `1.0.0-alpha04` to `1.0.0-alpha05`.

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "ru.resodostudio.muzyakich"
-        versionCode = 4
-        versionName = "1.0.0-alpha04"
+        versionCode = 5
+        versionName = "1.0.0-alpha05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
