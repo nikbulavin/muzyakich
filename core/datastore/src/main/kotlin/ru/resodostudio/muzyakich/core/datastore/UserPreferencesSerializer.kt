2ea6a312a08940d566ba158f2f30868a28104e96
Nikita Bulavin <nikbulavin@proton.me>
2026-04-26 22:36:06 +0300

    Refactor `UserPreferencesSerializer` to use `runCatching`
    This commit refactors the `readFrom` method in `UserPreferencesSerializer` to use Kotlin's `runCatching` builder instead of a standard `try-catch` block for more idiomatic error handling.
    
    Key changes:
    - Updated `readFrom` to use `runCatching` and `getOrElse` to catch parsing errors and throw `CorruptionException`.
    - Removed the unused `com.google.protobuf.InvalidProtocolBufferException` import.

--- core/datastore/src/main/kotlin/ru/resodostudio/muzyakich/core/datastore/UserPreferencesSerializer.kt
+++ core/datastore/src/main/kotlin/ru/resodostudio/muzyakich/core/datastore/UserPreferencesSerializer.kt
@@ -2,7 +2,6 @@ package ru.resodostudio.muzyakich.core.datastore
 
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
-import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 import javax.inject.Inject
@@ -14,12 +13,13 @@ internal class UserPreferencesSerializer @Inject constructor() : Serializer<User
 
     override val defaultValue: UserPreferences = getCustomInstance()
 
-    override suspend fun readFrom(input: InputStream): UserPreferences =
-        try {
+    override suspend fun readFrom(input: InputStream): UserPreferences {
+        return runCatching {
             UserPreferences.parseFrom(input)
-        } catch (exception: InvalidProtocolBufferException) {
+        }.getOrElse { exception ->
             throw CorruptionException("Cannot read proto.", exception)
         }
+    }
 
     override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
         t.writeTo(output)
