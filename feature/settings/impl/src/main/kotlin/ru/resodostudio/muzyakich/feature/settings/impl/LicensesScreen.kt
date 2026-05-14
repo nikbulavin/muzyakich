ae954d3458a07232bf63844be1540c4dd250d1c0
Nikita Bulavin <nikbulavin@proton.me>
2026-05-14 20:03:14 +0300

    Use LibraryActionMode and bump aboutLibraries
    Adapt LicensesScreen to the AboutLibraries 15.x API by replacing the removed showDescription param with actionMode = LibraryActionMode.Icons and adding the necessary import. Remove an unnecessary ExperimentalMaterial3Api opt-in. Also bump aboutLibraries to 15.0.0-a02 and ksp to 2.3.8 in libs.versions.toml to match the updated dependency.

--- feature/settings/impl/src/main/kotlin/ru/resodostudio/muzyakich/feature/settings/impl/LicensesScreen.kt
+++ feature/settings/impl/src/main/kotlin/ru/resodostudio/muzyakich/feature/settings/impl/LicensesScreen.kt
@@ -1,7 +1,6 @@
 package ru.resodostudio.muzyakich.feature.settings.impl
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipAnchorPosition
@@ -12,12 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.variant.LibraryActionMode
 import ru.resodostudio.muzyakich.core.designsystem.component.MuzIconButton
 import ru.resodostudio.muzyakich.core.designsystem.icon.MuzIcons
 import ru.resodostudio.muzyakich.core.designsystem.icon.rounded.ArrowBack
 import ru.resodostudio.muzyakich.core.locales.R as localesR
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun LicensesScreen(
     onBackClick: () -> Unit,
@@ -43,7 +42,7 @@ internal fun LicensesScreen(
             libraries = libraries,
             contentPadding = paddingValues,
             modifier = Modifier.fillMaxSize(),
-            showDescription = true,
+            actionMode = LibraryActionMode.Icons,
         )
     }
 }
