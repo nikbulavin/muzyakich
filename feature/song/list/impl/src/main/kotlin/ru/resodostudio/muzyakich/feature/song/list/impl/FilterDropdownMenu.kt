d7b6d22df5da9cccf70e751bfe3e84b591f22427
Nikita Bulavin <nikbulavin@proton.me>
2026-07-18 19:24:17 +0300

    Add ExperimentalMaterial3Api opt-in to FilterDropdownMenu
    - Add `@OptIn(ExperimentalMaterial3Api::class)` annotation to the `FilterDropdownMenu` composable.

--- feature/song/list/impl/src/main/kotlin/ru/resodostudio/muzyakich/feature/song/list/impl/FilterDropdownMenu.kt
+++ feature/song/list/impl/src/main/kotlin/ru/resodostudio/muzyakich/feature/song/list/impl/FilterDropdownMenu.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DropdownMenuGroup
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.DropdownMenuPopup
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MenuAnchorPosition
 import androidx.compose.material3.MenuDefaults
@@ -42,6 +43,7 @@ import ru.resodostudio.muzyakich.core.model.SortBy
 import ru.resodostudio.muzyakich.core.model.SortOrder
 import ru.resodostudio.muzyakich.core.locales.R as localesR
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun FilterDropdownMenu(
     filterConfig: FilterConfig,
