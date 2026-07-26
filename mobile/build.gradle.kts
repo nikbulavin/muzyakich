d1fa33c52633bda0a01fcbf63ad7e56b94229ec7
Nikita Bulavin <nikbulavin@proton.me>
2026-07-26 11:46:31 +0300

    Bump version to 1.1.0
    - Update `versionCode` to 10 in `mobile/build.gradle.kts`
    - Update `versionName` to 1.1.0 in `mobile/build.gradle.kts`

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "ru.resodostudio.muzyakich"
-        versionCode = 9
-        versionName = "1.0.0"
+        versionCode = 10
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
