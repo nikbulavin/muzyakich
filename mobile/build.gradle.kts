02758472cab36e31e11bcc2a95ec7c5475d450d5
Nikita Bulavin <nikbulavin@proton.me>
2026-08-13 09:41:39 +0300

    Bump version to 1.2.0 and update AndroidX Tracing to 2.0.0
    - Update `versionCode` to `11` and `versionName` to `1.2.0` in `mobile/build.gradle.kts`
    - Bump `androidxTracing` version to `2.0.0` in `libs.versions.toml`
    - Update `androidx.tracing` artifacts to `2.0.0` in `prodReleaseRuntimeClasspath.txt`

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "ru.resodostudio.muzyakich"
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
