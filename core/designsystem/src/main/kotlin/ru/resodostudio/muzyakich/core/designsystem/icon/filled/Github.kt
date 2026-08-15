714f09af92e894702703b1bb50d656b6663b517c
Nikita Bulavin <nikbulavin@proton.me>
2026-08-15 16:56:52 +0300

    Add "Source code" link to Settings and GitHub icon
    - Create `MuzIcons.Filled.Github` vector icon in the design system
    - Add "Source code" item to the About section in `SettingsScreen` linking to the repository URL
    - Update `segmentedShapes` indices in the About section list to reflect the increased item count (from 4 to 5)
    - Add `core_locales_source_code` string and translations for RU, JA, and KO
    - Correct capitalization for "Dynamic color" and "Privacy policy" in English strings
    - Fix punctuation for `core_locales_remove_from_playlist` in Japanese strings

---
+++ core/designsystem/src/main/kotlin/ru/resodostudio/muzyakich/core/designsystem/icon/filled/Github.kt
@@ -0,0 +1,59 @@
+package ru.resodostudio.muzyakich.core.designsystem.icon.filled
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import ru.resodostudio.muzyakich.core.designsystem.icon.MuzIcons
+
+val MuzIcons.Filled.Github: ImageVector
+    get() {
+        if (_Github != null) {
+            return _Github!!
+        }
+        _Github = ImageVector.Builder(
+            name = "Rounded.Github",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f,
+        ).apply {
+            path(fill = SolidColor(Color.Black)) {
+                moveTo(10.226f, 17.284f)
+                curveToRelative(-2.965f, -0.36f, -5.054f, -2.493f, -5.054f, -5.256f)
+                curveToRelative(0f, -1.123f, 0.404f, -2.336f, 1.078f, -3.144f)
+                curveToRelative(-0.292f, -0.741f, -0.247f, -2.314f, 0.09f, -2.965f)
+                curveToRelative(0.898f, -0.112f, 2.111f, 0.36f, 2.83f, 1.01f)
+                curveToRelative(0.853f, -0.269f, 1.752f, -0.404f, 2.853f, -0.404f)
+                curveToRelative(1.1f, 0f, 1.999f, 0.135f, 2.807f, 0.382f)
+                curveToRelative(0.696f, -0.629f, 1.932f, -1.1f, 2.83f, -0.988f)
+                curveToRelative(0.315f, 0.606f, 0.36f, 2.179f, 0.067f, 2.942f)
+                curveToRelative(0.72f, 0.854f, 1.101f, 2f, 1.101f, 3.167f)
+                curveToRelative(0f, 2.763f, -2.089f, 4.852f, -5.098f, 5.234f)
+                curveToRelative(0.763f, 0.494f, 1.28f, 1.572f, 1.28f, 2.807f)
+                verticalLineToRelative(2.336f)
+                curveToRelative(0f, 0.674f, 0.561f, 1.056f, 1.235f, 0.786f)
+                curveToRelative(4.066f, -1.55f, 7.255f, -5.615f, 7.255f, -10.646f)
+                curveTo(23.5f, 6.188f, 18.334f, 1f, 11.978f, 1f)
+                curveTo(5.62f, 1f, 0.5f, 6.188f, 0.5f, 12.545f)
+                curveToRelative(0f, 4.986f, 3.167f, 9.12f, 7.435f, 10.669f)
+                curveToRelative(0.606f, 0.225f, 1.19f, -0.18f, 1.19f, -0.786f)
+                verticalLineTo(20.63f)
+                arcToRelative(2.9f, 2.9f, 0f, isMoreThanHalf = false, isPositiveArc = true, -1.078f, 0.224f)
+                curveToRelative(-1.483f, 0f, -2.359f, -0.808f, -2.987f, -2.313f)
+                curveToRelative(-0.247f, -0.607f, -0.517f, -0.966f, -1.034f, -1.033f)
+                curveToRelative(-0.27f, -0.023f, -0.359f, -0.135f, -0.359f, -0.27f)
+                curveToRelative(0f, -0.27f, 0.45f, -0.471f, 0.898f, -0.471f)
+                curveToRelative(0.652f, 0f, 1.213f, 0.404f, 1.797f, 1.235f)
+                curveToRelative(0.45f, 0.651f, 0.921f, 0.943f, 1.483f, 0.943f)
+                curveToRelative(0.561f, 0f, 0.92f, -0.202f, 1.437f, -0.719f)
+                curveToRelative(0.382f, -0.381f, 0.674f, -0.718f, 0.944f, -0.943f)
+            }
+        }.build()
+
+        return _Github!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _Github: ImageVector? = null
