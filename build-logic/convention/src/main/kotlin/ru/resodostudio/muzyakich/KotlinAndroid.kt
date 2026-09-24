5bfc5c392fe8075e78607414f565e1ac397519c2
Nikita Bulavin <nikbulavin@proton.me>
2026-09-24 08:51:09 +0300

    Update dependencies and Android build configuration
    - Remove `compileSdkMinor = 2` from `KotlinAndroid.kt`
    - Bump `haze` to `2.0.0`, `androidxNavigation3` to `1.2.0`, along with `androidGradlePlugin`, `androidxTracing`, `coilBom`, and `protobuf` in `libs.versions.toml`
    - Replace `net.jthink:jaudiotagger` with `com.github.AdrienPoupa:jaudiotagger` and update other dependency classpath references in `prodReleaseRuntimeClasspath.txt`

--- build-logic/convention/src/main/kotlin/ru/resodostudio/muzyakich/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/ru/resodostudio/muzyakich/KotlinAndroid.kt
@@ -16,7 +16,6 @@ internal fun Project.configureKotlinAndroid(
 ) {
     commonExtension.apply {
         compileSdk = 37
-        compileSdkMinor = 2
 
         defaultConfig.apply {
             minSdk = 30
